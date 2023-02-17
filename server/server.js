f61ddce4dbfc236420a53313241fc32d3c8bb498
Mstrmindz <tunjexi@yahoo.com>
2023-02-17 16:31:05 +0100

    server file modification

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.post("/", async (req, res) => {
     });
   } catch (error) {
     console.error(error);
-    res.status(500).send(error || "Something went wrong");
+    res.status(500).send(error || "Oops, Something went wrong");
   }
 });
 
